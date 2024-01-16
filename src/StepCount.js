2bc3f0958b8492660d310ac95cd5135fbbff66d3
jamil Khan <jamilkhan.nu@gmail.com>
2024-01-16 01:48:38 +0000

    step count app

---
+++ src/StepCount.js
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+const messages = [
+  "Learn React ⚛️",
+  "Apply for jobs 💼",
+  "Invest your new income 🤑",
+];
+const StepCount = () => {
+  const [step, setStep] = useState("");
+  const [isOpen, setIsOpen] = useState(false);
+
+  function handlePrevious() {
+    if (step > 1) setStep(step - 1);
+  }
+  function handleNext() {
+    if (step < 3) setStep(step + 1);
+  }
+  function handleClose() {
+    setIsOpen(!isOpen);
+  }
+
+  return (
+    <>
+      <button onClick={handleClose} className="close">
+        x
+      </button>
+      {isOpen && (
+        <div className="steps">
+          <div className="numbers">
+            <div className={step >= 1 ? "active" : ""}>1</div>
+            <div className={step >= 2 ? "active" : ""}>2</div>
+            <div className={step >= 3 ? "active" : ""}>3</div>
+          </div>
+          <div className="message">
+            Step {step} : {messages[step - 1]}
+          </div>
+          <div className="buttons">
+            <button onClick={handlePrevious}>Previous</button>
+            <button onClick={handleNext}>Next</button>
+          </div>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default StepCount;
