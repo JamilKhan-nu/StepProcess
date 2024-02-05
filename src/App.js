0c26a86405b55e3c161052789f2571e9f5b00d09
jamil Khan <jamilkhan.nu@gmail.com>
2024-02-05 22:50:08 +0000

    added another line in feature

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ const App = () => {
   return (
     <>
       <h1>This is a step count</h1>
+      <h2>this is from feature branch</h2>
+      <h3>this is different from main</h3>
       <StepCount />
     </>
   );
